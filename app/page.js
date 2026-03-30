4d5d98704922449d9ae257eb89cf0e7cfd2a888a
haris0093 <harisiunc@gmail.com>
2026-03-30 22:00:02 +0500

    Remove crat

--- app/page.js
+++ app/page.js
@@ -448,7 +448,7 @@ export default function Home() {
             <table className="table table-striped table-hover">
               <thead style={{ backgroundColor: "#28a745", color: "white" }}>
                 <tr>
-                  <th>Brand crat</th>
+                  <th>Brand</th>
                   <th>Orders</th>
                   <th>Fulfilled</th>
                   <th>Partially <br></br>Refunded</th>
