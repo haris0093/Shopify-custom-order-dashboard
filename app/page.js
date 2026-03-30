5eda8ac2c9dc41bdce9534d6d4b45f4101a4802a
haris0093 <harisiunc@gmail.com>
2026-03-30 19:31:56 +0500

    removed

--- app/page.js
+++ app/page.js
@@ -321,7 +321,7 @@ export default function Home() {
                     value={pendingRange}
                     onChange={(e) => setPendingRange(e.target.value)}
                   >
-                    <option value="today">Today last</option>
+                    <option value="today">Today</option>
                     <option value="thisWeek">This Week</option>
                     <option value="thisMonth">This Month</option>
                     <option value="thisYear">This Year</option>
